92d3d78444155d4c7f95e05c773916c4b5820725
TheProjectsX <manvstech2.0@gmail.com>
2024-03-28 23:02:08 -0700

    Removed Table Dark Mode

--- src/components/ReadingTimeline.jsx
+++ src/components/ReadingTimeline.jsx
@@ -43,8 +43,8 @@ const ReadingTimeline = () => {
       {/* Timeline Data */}
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm rtl:text-right text-gray-500 text-center">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" className="px-6 py-3">
                 SL.
@@ -72,13 +72,10 @@ const ReadingTimeline = () => {
           <tbody>
             {timelineData.map((item, idx) => {
               return (
-                <tr
-                  key={idx}
-                  className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
-                >
+                <tr key={idx} className="odd:bg-white even:bg-gray-50 border-b">
                   <th
                     scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
                   >
                     {timelineData.length - idx}
                   </th>
@@ -94,7 +91,7 @@ const ReadingTimeline = () => {
                   <td className="px-6 py-4">
                     <Link
                       to={`/books/${item["bookId"]}`}
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      className="font-medium text-blue-600 hover:underline"
                     >
                       View Details
                     </Link>
